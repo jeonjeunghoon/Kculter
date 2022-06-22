61efb4e8e811bd894aa2944f14aed38c1e9c5b40
hankgood95 <hankgood958@gmail.com>
2022-06-22 16:15:38 +0900

    Hello World

---
+++ src/main/java/com/prac/react/ReactApplication.java
@@ -0,0 +1,13 @@
+package com.prac.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prac/react/ReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
