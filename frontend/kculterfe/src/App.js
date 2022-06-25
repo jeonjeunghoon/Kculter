dc0607843c7abbe091236cb7ed9c5d19bd122321
hankgood95 <hankgood958@gmail.com>
2022-06-26 03:35:25 +0900

    일단은 연동은 했는데 node 및 npm 버전은 일치시켜야 할거같아여~

--- frontend/kculterfe/src/App.js
+++ frontend/kculterfe/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [message, setMessage] = useState("");     
+  
+  useEffect(() => {
+    fetch('/test')
+    .then(response => response.text())
+    .then(message => {
+      setMessage(message);
+    });
+  },[])
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,13 +20,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className='App-title'>{message}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 안뇽
         </a>
       </header>
     </div>
