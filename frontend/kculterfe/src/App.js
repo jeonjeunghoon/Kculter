3a6226b12c74224c9273f70a9e35bfe87964b1e8
hankgood95 <hankgood958@gmail.com>
2022-06-29 20:31:50 +0900

    일단은 처음 시작화면 부분 수정했어용

--- frontend/kculterfe/src/App.js
+++ frontend/kculterfe/src/App.js
@@ -1,35 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import NavBar from './navbar/NavBar'
 
 function App() {
-  const [message, setMessage] = useState("");     
-  
-  useEffect(() => {
-    fetch('/test')
-    .then(response => response.text())
-    .then(message => {
-      setMessage(message);
-    });
-  },[])
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className='App-title'>{message}</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 안뇽
-        </a>
-      </header>
+    <div style={{border:"1px"}}>
+      <NavBar/>
     </div>
   );
 }
