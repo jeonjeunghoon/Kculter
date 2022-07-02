502a2e9a6d01389a8a34d884b4536853a9a48f4b
hankgood95 <hankgood958@gmail.com>
2022-07-02 13:06:55 +0900

    메인페이지 변경 및 네비바 생성

---
+++ frontend/kculterfe/src/main/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function MainPage(){
+    return(
+        <>
+            <div style={{border:'1px solid black'}}>
+                <h1>여기는 메인페이지얌</h1>
+            </div>
+        </>
+    );
+}
+export default MainPage;
--- frontend/kculterfe/src/navbar/NavBar.js
+++ frontend/kculterfe/src/navbar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-function Header(){
+import {Link} from 'react-router-dom';
+function NavBar(){
     const gotoLogin = function(){
         alert("로그인페이지로 가고싶다...")
     }
     return(
-        <>
-            <h1>여기는 네비바 구역</h1>
-            <button onClick={gotoLogin}>로그인</button>
-        </>
+        <div style={{display:'inline-block',width:'100%',border:"1px solid black"}}>
+            <h1 style={{float:"left"}}>Kculter logo</h1>
+            <Link to="/login">로그인</Link>
+        </div>
     );
 }
-export default Header;
+export default NavBar;
