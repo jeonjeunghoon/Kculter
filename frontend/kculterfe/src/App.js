5e261cdf039865da11eabaa7cd4e3902a11ff7c7
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-14 16:44:10 +0900

    Add: MapPage.js

--- frontend/kculterfe/src/App.js
+++ frontend/kculterfe/src/App.js
@@ -3,16 +3,15 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import NavBar from './navbar/NavBar';
 import Login from './login/LoginPage';
 import MainPage from './main/MainPage';
-import GoogleMap from './map/googleMap';
+import MapPage from './map/MapPage';
 
 function App() {
-	const gps = [{lat: 37.3379297, lng: 127.9269225}]; // MAP에 필요한 gps
   return (
     <BrowserRouter>
       <NavBar/>
       	<Routes>
         	<Route path="/login" element={<Login/>}></Route>
-			<Route path="/googleMap" element={<GoogleMap locations={gps}></GoogleMap>}></Route>
+			<Route path="/MapPage" element={<MapPage/>}></Route>
         	<Route path="/" element={<MainPage/>}></Route>
       	</Routes>
     </BrowserRouter>
--- frontend/kculterfe/src/map/MapNav.js
+++ frontend/kculterfe/src/map/MapNav.js
@@ -12,7 +12,7 @@ const MapNav = () => {
 				대신 Link tag를 사용한다.
 
 				*/}
-				<Link to="googleMap">Go to Map</Link>
+				<Link to="MapPage">Go to MapPage</Link>
 			</nav>
 		</div>
 	);
