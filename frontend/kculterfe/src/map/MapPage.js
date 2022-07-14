50d08780c6a9cce44ea0bb03f014f915729b31ae
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-14 19:44:45 +0900

    Update: Maps

---
+++ frontend/kculterfe/src/map/MapPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './MapPage.css';
+import GoogleMap from './GoogleMap';
+import { Link } from 'react-router-dom';
+
+const MapPage = () => {
+	const [search, setSearch] = useState("");
+	const onChange = (e) => {
+		setSearch(e.target.value);
+	}
+
+	const center = {
+			lat: 37.3379297,
+			lng: 127.9269225
+	};
+
+	return (
+		<div className='map-page'>
+			<nav>
+				<h1>NAV</h1>
+			</nav>
+			<header>
+				<h4>HEADER</h4>
+			</header>
+			<div className='search'>
+				<Link to='/'>
+					<button>Go Back</button>
+				</Link>
+				<input
+					type='text'
+					value='SEARCH'
+					onChange={onChange}
+				/>
+			</div>
+			<div className='content'>
+				<GoogleMap center={center}/>
+			</div>
+		</div>
+	);
+}
+
+export default MapPage;
