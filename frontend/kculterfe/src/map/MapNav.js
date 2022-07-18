834e016dd0aa6d985dcf512d15958408c4b83c09
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-18 21:41:48 +0900

    Debugging: async

--- frontend/kculterfe/src/map/MapNav.js
+++ frontend/kculterfe/src/map/MapNav.js
@@ -2,18 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MapNav = () => {
+	const center = {
+		lat: 0,
+		lng: 0
+	};
+	const getPos = () => {
+		return new Promise((resolve, reject) => {
+			navigator.geolocation.getCurrentPosition(resolve, reject);
+		});
+	}
+	const getGps = async() => {
+		if (navigator.geolocation) { // geolocation API 액세스 여부 확인
+			const pos = await getPos();
+			return {
+				lat: pos.coords.latitude,
+				lng: pos.coords.longitude
+			};
+		} else {
+			return {
+				lat: 37.5758772,
+				lng: 126.9768121
+			}
+		}
+	}
+	const run = (async() => {
+		const { lat, lng } = await getGps();
+		center.lat = lat;
+		center.lng = lng;
+		console.log(center);
+	})();
+
 	return (
 		<div style={{border: '1px solid black'}}>
 			<nav>
 				<h3>MAP Link</h3>
-				{/*
-
-				리액트는 a tag를 사용하지 않는다.
-				리액트 앱이 가진 상태가 초기화되고 랜더링된 컴포넌트들이 모두 날라가버린다.
-				대신 Link tag를 사용한다.
-
-				*/}
-				<Link to="MapPage">Go to MapPage</Link>
+				<Link to="MapPage"
+					state={{
+						center: center,
+					}
+				}>Go to MapPage</Link>
 			</nav>
 		</div>
 	);
