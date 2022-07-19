262436db25eb6cd4cffb52050b11776e16223609
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-19 15:44:03 +0900

    Update

--- frontend/kculterfe/src/map/MapPage.js
+++ frontend/kculterfe/src/map/MapPage.js
@@ -3,36 +3,60 @@ import './MapPage.css';
 import GoogleMap from './googleMap';
 import { Link, useLocation } from 'react-router-dom';
 
-const MapPage = () => {
+const MapNav = () => {
+	return (
+		<nav>
+			
+		</nav>
+	);
+}
+
+const MapHeader = () => {
+	return (
+		<header>
+			<h4>HEADER</h4>
+		</header>
+	);
+}
+
+const MapSearch = () => {
 	const [search, setSearch] = useState("");
 	const onChange = (e) => {
 		setSearch(e.target.value);
 	}
 
+	return (
+		<div className='search'>
+			<Link to='/'>
+				<button>Go Back</button>
+			</Link>
+			<input
+				type='text'
+				value={search}
+				onChange={onChange}
+			/>
+		</div>
+	);
+}
+
+const MapContent = () => {
 	const location = useLocation();
 	const { center } = location.state;
 
 	return (
+		<div className='content'>
+			<GoogleMap center={center} />
+		</div>
+	);
+}
+
+const MapPage = () => {
+	return (
 		<div className='map-page'>
-			<nav>
-				<h1>NAV</h1>
-			</nav>
-			<header>
-				<h4>HEADER</h4>
-			</header>
-			<div className='search'>
-				<Link to='/'>
-					<button>Go Back</button>
-				</Link>
-				<input
-					type='text'
-					value={search}
-					onChange={onChange}
-				/>
-			</div>
-			<div className='content'>
-				<GoogleMap center={center} />
-			</div>
+			<MapNav />
+			<MapHeader />
+			<MapSearch />
+			<MapContent />
 		</div>
 	);
 }
