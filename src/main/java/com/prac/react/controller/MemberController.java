0c6beff8596e125c6dda92a207cced7a181a05a6
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-19 13:32:47 +0900

    merge

--- src/main/java/com/prac/react/controller/MemberController.java
+++ src/main/java/com/prac/react/controller/MemberController.java
@@ -1,8 +1,7 @@
-package main.java.com.prac.react.controller;
+package com.prac.react.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.java.com.prac.react.model.dto.Member;
@@ -11,16 +10,15 @@ import main.java.com.prac.react.model.dto.Member;
  회원 정보와 관련된 일을 할때 들어올 Controller 입니다 */
 
 @RestController
-@RequestMapping("/member/")
 public class MemberController {
 
     //로그를 찍어보기 위해서 만든 인스턴스
 
-    @PostMapping("/{member}")
-    public int checkMember(@PathVariable("member") Member member){
+    @PostMapping("member")
+    public int checkMember(@RequestBody Member member){
         //여기서는 일단 로그인 버튼을 누른 사용자가 우리 사이트에 회원가입된 사용자인지 체크할것이다.
         //일단은 react에서 받은 정보를 한번 출력해보자.
-        System.out.println(member.toString());
+        System.out.println("Hello : "+ member.toString());
         return 1;
     }
 }
