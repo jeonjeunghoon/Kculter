566dfe8de83f24aaef8eeed15564fbd9b9a5e056
hankgood95 <hankgood958@gmail.com>
2022-07-23 23:57:05 +0900

    관리자페이지를 만들어봤습니다.

---
+++ frontend/kculterfe/src/manager/ManagerMain.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ManagerMain(){
+    return(
+        <div style={{border:"1px solid black"}}>
+            <h1>여기는 관리자 페이지</h1>
+        </div>
+    );
+}
+export default ManagerMain;
--- src/main/java/com/prac/react/model/dto/Member.java
+++ src/main/java/com/prac/react/model/dto/Member.java
@@ -4,6 +4,10 @@ public class Member{
     private int memberNum;
     private String email;
     private String mb_name;
+    private String country; //회원의 국가
+    private String pf_image; //회원의 프로필 이미지. 구글 이미지 혹은 본인 지정을 쓸 예정
+    private int status; //회원이 탈퇴한 회원인지 아닌지를 체크하기 위해
+    private boolean spv; //관리자인지 아닌지 체크하기 위해
     private boolean checkMember;
 
     public Member(){}
