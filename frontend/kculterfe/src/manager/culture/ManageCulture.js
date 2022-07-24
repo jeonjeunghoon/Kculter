3bc82451de7e90ed571eab813f76f2456178456f
hankgood95 <hankgood958@gmail.com>
2022-07-25 00:31:42 +0900

    관리자 페이지에서 중첩라우팅을 만들어봤습니다.

---
+++ frontend/kculterfe/src/manager/culture/ManageCulture.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function ManageCulture(){
+    return(
+        <div style={{border:"1px solid black"}}>
+            <h1>여기는 문화체험 관리</h1>
+        </div>
+    );
+}
+export default ManageCulture;
---
+++ frontend/kculterfe/src/manager/navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function NavBar() {
+  return (
+    <SNavBar>
+      <SListWrapper>
+        <SListItems>
+          <SListAnchor to="/">메인</SListAnchor>
+        </SListItems>
+        <SListItems>
+          <SListAnchor to="/manager/kpop">Kpop</SListAnchor>
+        </SListItems>
+        <SListItems>
+          <SListAnchor to="/manager/culture">문화체험</SListAnchor>
+        </SListItems>
+      </SListWrapper>
+    </SNavBar>
+  );
+}
+
+const SNavBar = styled.ul`
+  background-color: white;
+  padding: 20px;
+`;
+
+const SListWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const SListItems = styled.li`
+  list-style: none;
+  padding-right: 15px;
+`;
+
+const SListAnchor = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
