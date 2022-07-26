ba54e3356a3b55525445735a129c03c1faeb8f0c
guswlsakfls <guswlsakfls@gmail.com>
2022-07-26 16:35:47 +0900

    Delete: Mbody.js 파일 하드코딩한거 삭제

--- frontend/kculterfe/src/myPage/MBody.js
+++ frontend/kculterfe/src/myPage/MBody.js
@@ -14,39 +14,6 @@ function MBody(props) {
             </div>
             {/* 저장한 경로 리스트 */}
             <BoxCardList/>
-            <Card className="mx-5">
-                <Row>
-                    <Col sm={2} className="bg-primary">
-                        <div>
-                            <p className="mx-2 py-2">
-                                02.<br />Second day
-                            </p>
-                            <p className="mx-3 position-absolute bottom-0">
-                                + -
-                            </p>
-                        </div>
-                    </Col>
-                    <Col sm={10}>
-                        <Row className="mx-5">
-                            <Col className="py-5">
-                                <Card style={{ width: '6rem', height: '6rem' }}>
-                                    EXO
-                                </Card>
-                            </Col>
-                            <Col className="py-5">
-                                <Card style={{ width: '6rem', height: '6rem' }}>
-                                    BTS
-                                </Card>
-                            </Col>
-                            <Col className="py-5">
-                                <Card style={{ width: '6rem', height: '6rem' }}>
-                                    HANBOK
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Card>
     </div>
     )
 }
