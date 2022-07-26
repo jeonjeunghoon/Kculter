3806c22fa7d1737be3cb27330049ad981ac08a78
guswlsakfls <guswlsakfls@gmail.com>
2022-07-27 01:29:29 +0900

    fix: body-navbar 수정

--- frontend/kculterfe/src/myPage/components/MBody.js
+++ frontend/kculterfe/src/myPage/components/MBody.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Card, Button, Row, Col, Container } from 'react-bootstrap';
 import '../styles/MBody.css';
 import BoxCardList from './card/BoxCardList';
 
-function MBody(props) {
+function MBody() {
     return (
         <div id='my-body'>
             {/* 저장한 경로와 좋아요 리스트 선택 경로 네비*/}
-            <div className="my-3 body-navbar">
-                <button href="#" className="px-5 body-navbtn">My map</button>
-                <button href="#" className="px-5 body-navbtn">Like list</button>
-            </div>
+            <Card className="mb-3 body-navbar">
+                <Row>
+                    <Col sm={2} className="px-0 text-center" id='my-body'>
+                        <button href="#" className="body-navbtn">My map</button>
+                    </Col>
+                    <Col sm={2} className="px-0 text-center" id='my-body'>
+                        <button href="#" className="body-navbtn">Like list</button>
+                    </Col>
+                </Row>
+            </Card>
             {/* 저장한 경로 리스트 */}
             <BoxCardList/>
-    </div>
+        </div>
     )
 }
 
