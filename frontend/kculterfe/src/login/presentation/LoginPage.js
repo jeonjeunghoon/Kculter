d0d8354225b3340be867e8b72ff1972e8f66fa8c
jham <irang4605@naver.com>
2022-07-29 01:06:07 +0900

    main-login

--- frontend/kculterfe/src/login/presentation/LoginPage.js
+++ frontend/kculterfe/src/login/presentation/LoginPage.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './LoginPage.css';
 import GoogleLoginBtn from '../container/GoogleLoginBtn';
+import loginimg from '../../src_asset/로그인배경.png';
+import loginblack from '../../src_asset/loginblack.png';
+import logo from '../../src_asset/logo.png';
+import Loginlogo from '../presentation/Loginlogo';
 
 
 function LoginPage(){
     return(
-        <div style={{border:"1px solid black"}}>
-            <h1>여기는 로그인 페이지</h1>
-            <GoogleLoginBtn/>
+        <div className='login-con'>
+          <img src={loginimg} />
+          <div className='login-top'>
+          <Loginlogo className='loginNav'/>
+          </div>
+            <div className='login-div'>
+                <div className='login-black'>
+                    <img src={loginblack}></img>
+                </div>
+                <div>
+                  <GoogleLoginBtn/>
+                </div>
+            </div>
         </div>
     );
 }
