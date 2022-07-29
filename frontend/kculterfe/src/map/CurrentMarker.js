61655559d5dcc16ef5b2e00e62fda2c4dd33b2e6
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-07-29 15:58:57 +0900

    Modify: function name

---
+++ frontend/kculterfe/src/map/CurrentMarker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CurrentMarker = props => {
+	return (
+		<div className='current-marker-out'>
+			<div className='current-marker-in' />
+		</div>
+	);
+} 
+
+export default CurrentMarker;
--- frontend/kculterfe/src/map/MapLink.js
+++ frontend/kculterfe/src/map/MapLink.js
@@ -1,98 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
-const GetPos = () => {
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject);
-	});
-}
+import * as SetUserInfo from './SetUserInfo';
+import * as TourApi from './TourApi';
 
-const GetGps = async() => {
-	if (navigator.geolocation) { // geolocation API 액세스 여부 확인
-		const pos = await GetPos();
-		return {
-			lat: pos.coords.latitude,
-			lng: pos.coords.longitude
-		};
-	} else {
-		console.log('geolocation X');
-		return {
-			lat: 37.5758772,
-			lng: 126.9768121
-		}
-	}
-}
-
-const GetCurPos = async(center) => {
-	const { lat, lng } = await GetGps();
-	center.lat = lat;
-	center.lng = lng;
-}
-
-const GetLang = props => {
-	let input = prompt('언어 코드 입력');
-	if (input) {
-		props.language = input;
-		props.region = input;
-	} else {
-		props.language = 'en';
-		props.region = 'en';
-	}
-}
-
-const GetAreaCodeApi = () => {
-	let xhr = new XMLHttpRequest();
-	let url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchStay'; /* 필수 항목 URL */
-	let queryParams = '?' + encodeURIComponent('serviceKey') + '='+'3Z%2FYQWOyIAR89XtBFrgHdHGxDwSP12fVxUYyqy5VxpHHRNUVhYp3U9ptrdhgHFQ8OnEmPidWt4MZl%2BZlv70L%2Bw%3D%3D'; /*Service Key*/
-	
-	// 다른 api와 비교하여 이 부분만 작성법이 다르다.
-	queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-	queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-	queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC'); /**/
-	queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('Kculter'); /**/
-	queryParams += '&' + encodeURIComponent('areaCode') + '=' + encodeURIComponent('1'); /**/
-	
-	xhr.open('GET', url + queryParams);
-	xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-    }
-	};
-
-	xhr.send('');
-}
-
-const GetStayApi = props => {
-	let xhr = new XMLHttpRequest();
-	let url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchStay'; /* 필수 항목 URL */
-	let queryParams = '?' + encodeURIComponent('serviceKey') + '='+'3Z%2FYQWOyIAR89XtBFrgHdHGxDwSP12fVxUYyqy5VxpHHRNUVhYp3U9ptrdhgHFQ8OnEmPidWt4MZl%2BZlv70L%2Bw%3D%3D'; /*Service Key*/
-	
-	// 다른 api와 비교하여 이 부분만 작성법이 다르다.
-	queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
-	queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-	queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC'); /**/
-	queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest'); /**/
-	queryParams += '&' + encodeURIComponent('arrange') + '=' + encodeURIComponent('A'); /**/
-	queryParams += '&' + encodeURIComponent('listYN') + '=' + encodeURIComponent('Y'); /**/
-	queryParams += '&' + encodeURIComponent('areaCode') + '=' + encodeURIComponent(''); /**/
-	queryParams += '&' + encodeURIComponent('sigunguCode') + '=' + encodeURIComponent(''); /**/
-	queryParams += '&' + encodeURIComponent('hanOk') + '=' + encodeURIComponent(''); /**/
-	queryParams += '&' + encodeURIComponent('benikia') + '=' + encodeURIComponent(''); /**/
-	queryParams += '&' + encodeURIComponent('goodStay') + '=' + encodeURIComponent(''); /**/
-	queryParams += '&' + encodeURIComponent('modifiedtime') + '=' + encodeURIComponent(''); /**/
-	
-	xhr.open('GET', url + queryParams);
-	xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-    }
-	};
-
-	xhr.send('');
-}
-
-const MapLink = () => {
+function MapLink() {
 	const defaultProps = {
 		center: {
 			lat: 0,
@@ -102,12 +14,11 @@ const MapLink = () => {
 		region: null,
 		zoom: 15,
 	}
-	const [areaCode, setAreaCode] = useState('B02010100');
 	
-	GetCurPos(defaultProps.center); // 현재 위치 좌표 받아오기 (비동기 이슈 해결 X)
-	// GetLang(defaultProps); // 언어 설정 함수
-	GetAreaCodeApi(); // 지역코드 API 호출
-	GetStayApi(areaCode); // 숙박 API 호출
+	SetUserInfo.setCurPos(defaultProps.center); // 현재 위치 좌표 받아오기 (비동기 이슈 해결 X)
+	// SetUserInfo.setLanguage(defaultProps); // 언어 설정 함수
+	TourApi.getAreaCode(); // 지역코드 API 호출
+	TourApi.getStay(); // 숙박 API 호출
 
 	return (
 		<nav style={{border: '1px solid black'}}>
