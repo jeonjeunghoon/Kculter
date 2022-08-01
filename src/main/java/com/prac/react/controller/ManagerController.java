ea20f7f47f599e9308ebd5ad4f37a9bcdaa0ac62
hankgood95 <hankgood958@gmail.com>
2022-08-02 02:44:27 +0900

    파일정보 프론트단에서 백엔드 단으로 넘겼음.

--- src/main/java/com/prac/react/controller/ManagerController.java
+++ src/main/java/com/prac/react/controller/ManagerController.java
@@ -23,16 +23,20 @@ public class ManagerController{
     Date date = new Date();
     SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss"); //파일명을 현재시간으로 바꿔주기 위해서 사용하는 인스턴스입니다.
 
+    //@RequestPart는 multipart/form-data를 받기위해서 사용하는 어노테이션이다.
     @PostMapping("/cultureinfo")
     public int insertCultureInfo(@RequestPart("formValue") Culture culture,@RequestPart("file") MultipartFile mpf){
         logger.info("문화 저장 들어옴");
         logger.info("culture : "+ culture.toString());
-        logger.info("culture : "+ mpf.toString()); //여기서 에러가 났음
-        //여기서 이제 해야할것은 받은 파일명을 다른 파일명으로 저장을 해줘야함
-        //그러기 위해선 파일명을 변경해주는 작업이 진행되어야 함
-        int extension = culture.getFileUrl().lastIndexOf("."); //확장자 명이 시작되는 위치
-        String newFileName = sdf.format(date) + culture.getFileUrl().substring(extension); //바뀐이름.확장자 이렇게 바뀌게 됨
-        logger.info("changed file name : "+newFileName);
+        logger.info("culture : "+ mpf.toString()); 
+
+        //일단은 오리지널 파일명을 받아오자.
+        String originalFileName = mpf.getOriginalFilename();
+        logger.info("original file name : "+ originalFileName);
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        //저장될 파일명을 현재날짜시분초로 변경한다.
+        String savedFileName = sdf.format(date)+extension;
+        logger.info(savedFileName);
 
         //이제 파일명을 바꿨으니 이제 해야할일은 aws s3에 저장을 하는일이 남았다.
 
