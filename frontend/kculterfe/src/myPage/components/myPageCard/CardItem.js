9cf39d77549659c2af2656ef5a5acf222ee2a2fb
guswlsakfls <guswlsakfls@gmail.com>
2022-08-02 10:53:11 +0900

    update: 카드 호버 css 수정

--- frontend/kculterfe/src/myPage/components/myPageCard/CardItem.js
+++ frontend/kculterfe/src/myPage/components/myPageCard/CardItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { useState } from 'react';
+import { Card, Button, Modal } from 'react-bootstrap';
 import "../../styles/Card.css"
 
 function CardItem({ props }) {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 	return (
 		<>
 			<div className="mx-1 my-5">
 				<Card className="body-card">
+					{/* 카드 호버 기능 */}
+					<Card className="body-card-hover text-center" onClick={handleShow}>
+						<div className="square border border-1 rounded-pill m-auto" style={{ color: 'white', width: '6em'}}>
+							<span className="px-2" style={{ width: '20px'}}>
+								view more
+							</span>
+						</div>
+					</Card>
+					{/* 아이돌 카드 */}
 					<Card.Img variant="top" src={ props.img } style={{ height: '6em' }}/>
 					<Card.Body className="body-card-body">
 						<Card.Title className="text-center">
@@ -15,6 +29,24 @@ function CardItem({ props }) {
 					</Card.Body>
 				</Card>
 			</div>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+				<Modal.Title>{props.title}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<img width="200" height="300" src={props.img}></img>
+					{props.title}
+				</Modal.Body>
+				<Modal.Footer>
+				<Button variant="secondary" onClick={handleClose}>
+					Close
+				</Button>
+				<Button variant="primary" onClick={handleClose}>
+					Save Changes
+				</Button>
+				</Modal.Footer>
+			</Modal>
 		</>
 	);
 }
