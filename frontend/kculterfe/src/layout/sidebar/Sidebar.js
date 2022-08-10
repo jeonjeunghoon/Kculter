efdc685574c6a4512a780807b29bdc17e4f6620c
jeujeon <1132jjw@naver.com>
2022-08-10 14:38:59 +0900

    branch check

--- frontend/kculterfe/src/layout/sidebar/Sidebar.js
+++ frontend/kculterfe/src/layout/sidebar/Sidebar.js
@@ -17,6 +17,7 @@ export default function Sidebar(props) {
 			<br></br>
 			<br></br>
 			<br></br>
+			<br></br>
 			{ props.items.map((item, index) => <SidebarItem key={index} item={item} {...item} />)}
 		</div>
 	)
