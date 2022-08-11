a9aa8fba11b913993eb2169d7fda6388216abe2f
guswlsakfls <guswlsakfls@gmail.com>
2022-08-11 20:52:27 +0900

    Update: 박스리스트 스크롤 기능, 바디 네비바 색깔선택 변경

--- frontend/kculterfe/src/myPage/components/myPageCard/BoxCardItem.js
+++ frontend/kculterfe/src/myPage/components/myPageCard/BoxCardItem.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Card, Row, Col, Button } from 'react-bootstrap';
-import "../../styles/Card.css"
+import "../../styles/MyPageCard.css"
+import "../../styles/Button.css"
 import CardList from './CardList';
 
 function BoxCardItem({ props }) {
 	return (
 		<>
-            <Card className="mx-5 mb-3" style={{ width: '1200px'}}>
+            <Card className="box-card" style={{ width: '1200px'}}>
                 <Row>
                     <Col xs={2} className="bg-primary">
                         <p className="mx-2 py-2 text-light">
                             { props.dayNum }<br/>{ props.dayStr }
                         </p>
                         <p className="mx-2 position-absolute bottom-0">
-                            <Button variant="outline-danger btn-sm" className="btn border" style={{ width: '2em' }}>
-                                <span className="text-white">
-                                    +
-                                </span>
-                            </Button>
-                            <Button variant="outline-danger btn-sm" className="btn border" style={{ width: '2em' }}>
-                                <span className="text-white">
-                                    -
-                                </span>
-                            </Button>
+                            <button className="btn" onclike="">
+                                +
+                            </button>
+                            <button className="btn" onclike="">
+                                -
+                            </button>
                         </p>
                     </Col>
                     <Col xs={10}>
