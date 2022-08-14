adec82c79ed0a80b9a095ce6712ef5b08f7c89cf
guswlsakfls <guswlsakfls@gmail.com>
2022-08-15 04:00:22 +0900

    Fix: 모바일 화면일 때 부트스트랩 적용

--- frontend/kculterfe/src/myPage/components/myPageCard/BoxCardItem.js
+++ frontend/kculterfe/src/myPage/components/myPageCard/BoxCardItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Col, Button } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import "../../styles/MyPageCard.css"
 import "../../styles/Button.css"
 import CardList from './CardList';
@@ -7,13 +7,13 @@ import CardList from './CardList';
 function BoxCardItem({ props }) {
 	return (
 		<>
-            <Card className="box-card" style={{ width: '1200px'}}>
+            <Card className="box-card">
                 <Row>
-                    <Col xs={2} className="bg-primary">
+                    <Col md={2} className="bg-primary box-card-top">
                         <p className="mx-2 py-2 text-light">
                             { props.dayNum }<br/>{ props.dayStr }
                         </p>
-                        <p className="mx-2 position-absolute bottom-0">
+                        <p className="box-btn">
                             <button className="btn" onclike="">
                                 +
                             </button>
@@ -22,7 +22,7 @@ function BoxCardItem({ props }) {
                             </button>
                         </p>
                     </Col>
-                    <Col xs={10}>
+                    <Col md={10} className="">
                         <div className="date">
                             <span className="date-span">
                                 { props.date }
