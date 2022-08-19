3e63249f9c7d71615d0353f688ee06b1eae2e13f
jham <irang4605@naver.com>
2022-08-20 00:13:14 +0900

    up

--- frontend/kculterfe/src/login/container/EmailCheck.js
+++ frontend/kculterfe/src/login/container/EmailCheck.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export function checkEmail(email){
-  axios.get("/member/emaildup?email="+email)
+  axios.get("/member/emaildup?email="+email) //조회시 사용, 중복검사 
   .then(function(res){
-    if(res == 1){
+    if(res.data == 1){
       alert("사용가능한 이메일 입니다.");
     }else{
       alert("사용 불가능한 이메일 입니다.");
