9063a113859da9ef13132c84e3f62ea6f914c528
hankgood95 <hankgood958@gmail.com>
2022-08-19 17:41:30 +0900

    핀정보 가져오는거 만들었습니다.

--- src/test/java/com/prac/react/service/PinServiceTest.java
+++ src/test/java/com/prac/react/service/PinServiceTest.java
@@ -1,15 +1,51 @@
 package com.prac.react.service;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.prac.react.model.dto.Pin;
 
 @SpringBootTest
 @Transactional
 public class PinServiceTest {
 
+    Logger logger = LoggerFactory.getLogger(PinServiceTest.class);
+
+    @Autowired
+    PinService ps;
+
     @Test
     void testGetKpopPin() {
+        //given
+        int key = 1;
+        Pin pin = new Pin();
+        //when
+        pin = ps.getKpopPin(key);
+        //then
+        if(pin == null){
+            logger.info("Pin is null");
+        }else{
+            logger.info("Pin info : "+pin.toString());    
+        }
+    }
 
+    @Test
+    void testGetCulturePin(){
+        //given
+        int key = 1;
+        Pin pin = new Pin();
+        //when
+        pin = ps.getCulturePin(key);
+        //then
+        if(pin == null){
+            logger.info("Pin is null");
+        }else{
+            logger.info("Pin info : "+pin.toString());    
+        }
+              
     }
 }
