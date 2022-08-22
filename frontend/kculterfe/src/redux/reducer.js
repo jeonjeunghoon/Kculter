f981e2b3963a271ae62ba237ca03a640dba14ff2
jeujeon <1132jjw@naver.com>
2022-08-22 19:54:01 +0900

    sidebar page rerander

--- frontend/kculterfe/src/redux/reducer.js
+++ frontend/kculterfe/src/redux/reducer.js
@@ -2,11 +2,17 @@
 const initState = {
 	place: {},
 	course: [],
+	idolSelected: true,
+	attrSelected: false,
+	concertSelected: false,
 }
 
 // 액션 추가 여기
 export const CLICK_MARKER = 'CLICK_MARKER';
 export const MODIFY_COURSE = 'MODIFY_COURSE';
+export const SIDE_SET_IDOL = 'SIDE_SET_IDOL';
+export const SIDE_SET_ATTR = 'SIDE_SET_ATTR';
+export const SIDE_SET_CONCERT = 'SIDE_SET_CONCERT';
 
 // reducer
 export default function reducer(state = initState, action) {
@@ -21,6 +27,21 @@ export default function reducer(state = initState, action) {
 			return {
 				...state,
 				course: action.data,
+			}
+		case SIDE_SET_IDOL:
+			return {
+				...state,
+				idolSelected: action.data,
+			}
+		case SIDE_SET_ATTR:
+			return {
+				...state,
+				attrSelected: action.data,
+			}
+		case SIDE_SET_CONCERT:
+			return {
+				...state,
+				concertSelected: action.data,
 			}
 		default:
 			return state;
