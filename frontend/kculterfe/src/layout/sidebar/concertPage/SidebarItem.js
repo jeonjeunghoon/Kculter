872d02ec7b978e453abdf94d679e0e202ace1202
jeujeon <1132jjw@naver.com>
2022-08-22 16:24:33 +0900

    prepare for use redux

--- frontend/kculterfe/src/layout/sidebar/concertPage/SidebarItem.js
+++ frontend/kculterfe/src/layout/sidebar/concertPage/SidebarItem.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useDebugValue, useState } from "react";
+import { useDispatch } from "react-redux";
+import { SIDEBAR_SELECT_CARD } from "../../../redux/reducer";
 
 export default function SidebarItem(props) {
+	const dispatch = useDispatch();
 	const [open, setOpen] = useState(false);
 	const [select, setSelect] = useState(false);
 
+	dispatch({
+		type: SIDEBAR_SELECT_CARD,
+		data: open,
+	})
 	if (select == true)
 		props.changeSelect(false);
 	console.log(select);
