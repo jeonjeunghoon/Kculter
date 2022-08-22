a9fefe73e4f76fdc559fad3b3697d69643fe43e9
hankgood95 <hankgood958@gmail.com>
2022-08-22 17:12:12 +0900

    member 정보 redux에 저장

--- frontend/kculterfe/src/redux/reducer.js
+++ frontend/kculterfe/src/redux/reducer.js
@@ -2,6 +2,16 @@
 const initState = {
 	place: {},
 	course: [],
+	member:{
+		memberNum : 1,
+		email : 'hankgood95@naver.com',
+		pwd : '1234',
+		nickName : 'passcucci',
+		countryCode : 'KR',
+		age : 28,
+		gender : 'male',
+		pf_image: 'https://kculter-image.s3.ap-northeast-2.amazonaws.com/user.png'
+	}
 }
 
 // 액션 추가 여기
