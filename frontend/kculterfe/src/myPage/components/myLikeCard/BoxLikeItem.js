2f707d2a6514e3cb82c56f9411e66550233e0680
guswlsakfls <guswlsakfls@gmail.com>
2022-08-23 11:13:50 +0900

    Design: LikeList 카드 반응형 수정, Db 데이터 받음 - 창이 줄어들면 카드가 여백이 생기고 줄이 하나 줄어드는 것을 카드가 잘리다가 한줄정도 너비로 줄어들면 반응하게끔 수정 - Db 데이터를 받아서 적용함. 아직 likeList컬럼이 없어서 idolList로   받아옴 추후 수정예정

--- frontend/kculterfe/src/myPage/components/myLikeCard/BoxLikeItem.js
+++ frontend/kculterfe/src/myPage/components/myLikeCard/BoxLikeItem.js
@@ -5,16 +5,16 @@ function BoxLikeItem({ props }) {
 	return (
 		<div id="card-container">
 			<div className="image-container">
-				<img src={props.img} />
-				<div className="image-title"><h3>{props.title}</h3></div>
+				<img src={props.fileUrl} />
+				<div className="image-title"><h3>{props.name}</h3></div>
 			</div>
 			<div className="card-content">
 				<div className="like-num">
-					<img src={props.img} />
+					<img src={props.fileUrl} />
 					<p>likeImg<br></br>Likes</p>
 				</div>
 				<div className="spot-num">
-					<img src={props.img} />
+					<img src={props.file} />
 					<p>spotNum<br></br>Likes</p>
 				</div>
 			</div>
