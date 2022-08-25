e7afbba90c33935635394e2dc905af4fbf5925d5
jeujeon <1132jjw@naver.com>
2022-08-25 16:01:48 +0900

    add sidebar in mypage

--- frontend/kculterfe/src/myPage/page/MyPage.js
+++ frontend/kculterfe/src/myPage/page/MyPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import MyPageBody from '../components/MyPageBody';
+import { useSelector } from 'react-redux';
 
 function MyPage() {
-  return (
-    <>
-      <MyPageBody />
-    </>
-  );									
+  const dashboardSelected = useSelector(state => state.dashboardSelected);
+  const settingSelected = useSelector(state => state.settingSelected);
+
+  if (dashboardSelected) {
+    return (
+      <>
+        <MyPageBody />
+      </>
+    );	
+  }
+  else if (settingSelected) {
+    return (
+      <>
+        setting page
+      </>
+    );		
+  }							
 }
 
 export default MyPage;
