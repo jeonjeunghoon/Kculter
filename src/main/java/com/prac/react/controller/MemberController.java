38a5e5a0604149c8823db09a072cf75da3ca4299
hankgood95 <hankgood958@gmail.com>
2022-08-26 17:33:08 +0900

    회원가입 비밀번호 부분 수정

--- src/main/java/com/prac/react/controller/MemberController.java
+++ src/main/java/com/prac/react/controller/MemberController.java
@@ -67,8 +67,9 @@ public class MemberController {
         logger.info(member.toString());
 
         member.setPf_image("https://kculter-image.s3.ap-northeast-2.amazonaws.com/user.png");
-        String pwd = encrypt.shaEncryption(member.getPwd());
-        member.setPwd(pwd);
+        String pwd = encrypt.aesDecrypt(member.getPwd()); //대칭키로 복호화
+        String enccryptPwd = encrypt.shaEncryption(pwd); //복호화 한걸 암호화
+        member.setPwd(enccryptPwd);
 
         logger.info("Member : " + member.toString());
 
