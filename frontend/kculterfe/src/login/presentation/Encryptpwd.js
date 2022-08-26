8b9f692a46634cea1711661ed1e0e132c2ff0bf9
hankgood95 <hankgood958@gmail.com>
2022-08-26 17:38:44 +0900

    암호화 부분 수정

--- frontend/kculterfe/src/login/presentation/Encryptpwd.js
+++ frontend/kculterfe/src/login/presentation/Encryptpwd.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import cryptoJS from 'crypto-js'
 
-export function hashPwd(email,pwd) {
+export function hashPwd(info) {
   
   const secretKey = process.env.REACT_APP_AES_SECRET_KEY; //비밀키
   const iv = 'abcdefghijklmnop' // 16자리 iv
 
-  const autho = cryptoJS.AES.encrypt(email+"/"+pwd, cryptoJS.enc.Utf8.parse(secretKey), {
+  const autho = cryptoJS.AES.encrypt(info, cryptoJS.enc.Utf8.parse(secretKey), {
     iv: cryptoJS.enc.Utf8.parse(iv),
     padding: cryptoJS.pad.Pkcs7,
     mode: cryptoJS.mode.CBC,
