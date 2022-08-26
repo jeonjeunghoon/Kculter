ac7541b8a9ad72ec46d5f1cf9337ac91ceae7db8
guswlsakfls <guswlsakfls@gmail.com>
2022-08-26 22:48:57 +0900

    feat: 코스 불러오는 기능 추가.
    - 디비 데이터 코스 불러오고 콘솔에서 확인 가능.

--- frontend/kculterfe/src/myPage/container/GetCours.js
+++ frontend/kculterfe/src/myPage/container/GetCours.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getCourseList() {
     let list;
     //await 한 값을 보내준다.
-    return await axios.get('/places', {
+    return await axios.get('/course/1', {
         responseType: 'json'
     })
     .then(function(res){
