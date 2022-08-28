094ac1c3e5ff520d8a6a2164da8c5c711ae1e8f2
hyunhole <hyunhole@student.42seoul.kr>
2022-08-29 00:18:39 +0900

    IdolCard 디자인 수정

--- frontend/kculterfe/src/IdolListPage/Components/container/GetKpopListData.js
+++ frontend/kculterfe/src/IdolListPage/Components/container/GetKpopListData.js
@@ -16,4 +16,3 @@ export async function getKpopList() {
         alert("서버 통신 실패");
     });
 }
-
--- frontend/kculterfe/src/IdolListPage/Components/presentation/IdolCard.js
+++ frontend/kculterfe/src/IdolListPage/Components/presentation/IdolCard.js
@@ -33,11 +33,11 @@ function IdolCard( {keyNum, type, path_photo, title, num_like, num_spot, path_ma
 				<div className={styles.card_container}>
 					<div className={styles.image_container}>
 						<img src={path_photo} />
-					</div>
-					<div className={styles.card_content}>
 						<div className={styles.image_title}>
 							<h3>{title}</h3>
 						</div>
+					</div>
+					<div className={styles.card_content}>
 						<div className={styles.pin}>
 							<div className={styles.like_num}>
 								<p>{num_like }</p>
@@ -58,7 +58,6 @@ function IdolCard( {keyNum, type, path_photo, title, num_like, num_spot, path_ma
 				className={styles.modal}
 				show={show}
 				onHide={handleClose}
-				size="sm"
 			>
 				<Modal.Header className={styles.modal_header}>
 					<img src={path_photo} />
