5d3de212c73cfeb58852e9120f5a1443af874ed0
hyunho lee <sailor3947@gmail.com>
2022-08-30 22:51:41 +0900

    IdolList pin 반영, CultureList pin 오류 수정중

--- frontend/kculterfe/src/IdolListPage/Components/presentation/IdolList.js
+++ frontend/kculterfe/src/IdolListPage/Components/presentation/IdolList.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import IdolCard from './IdolCard.js';
 import IdolSearchBar from './IdolSearchBar';
 import { getKpopList } from '../container/GetKpopListData';
-import { getPinList } from '../container/GetPinListData.js';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 /** css module */
 import styles from './IdolList.module.css';
@@ -14,36 +13,28 @@ function IdolList({}) {
 	}
 
 	const [data, setData] = useState([]);
-	const [pinList, setPinList] = useState([]);
 	useEffect(() => {
 		getKpopList()
 		.then(resData => {
-			setData(resData)
+			setData(resData);
 		})
 		.catch(err => {
-			console.log(err);
-		});
-		console.log(data);
-		// getPinList()
-		// .then(resPinList => {
-		// 	setPinList(resPinList)
-		// })
-		// .catch(err => {
-		// 	console.log(err);
-		// });
-		// console.log(pinList);
+			console.log(err)
+		})
 	}, []);
+
 	const filterTitle = data.filter((p) => {
 		return p.name.replace(" ", "").toLocaleLowerCase().includes(search.toLocaleLowerCase());
 	});
+
 	return (
 		<div className={styles.ListBody}>
 			<div className={styles.SearchBar}>
 				<IdolSearchBar value={search} onChange={onChange} />
 			</div>
 			<div className={styles.CardContainer}>
-				{ filterTitle.map(idolcard => 
-				<div className={styles.CardDiv}>
+				{ filterTitle.map(idolcard => {
+					return <div className={styles.CardDiv}>
 					<IdolCard
 					keyHash={idolcard.keyHash}
 					type={1}
@@ -51,10 +42,9 @@ function IdolList({}) {
 					path_photo={idolcard.fileUrl}
 					num_like={idolcard.likeCount}
 					num_spot={idolcard.likeCount}
-					path_map={idolcard.path_map}
 					explain={idolcard.explain}
 					/>
-				</div>) }
+				</div>;}) }
 			</div>
 		</div>
 	)
