fc71505bcd7c3630a764ddd5679305c71ac59e1d
guswlsakfls <guswlsakfls@gmail.com>
2022-09-01 01:33:18 +0900

    Fix: course memberNum -> memberHash 로 변경

--- frontend/kculterfe/src/myPage/container/GetCours.js
+++ frontend/kculterfe/src/myPage/container/GetCours.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 export async function getCourseList() {
     let list;
     // /course/{회원번호}, {회원번호}에 memeber나 session? 에서 정보값을 가지고 와야함
-    const memberNum = window.sessionStorage.getItem("memberNum")
+    const memberHash = window.sessionStorage.getItem("memberHash")
 
-    return await axios.get('/course/'+ 1, {
+    return await axios.get('/course/'+ memberHash, {
         responseType: 'json'
     })
     .then(function(res){
