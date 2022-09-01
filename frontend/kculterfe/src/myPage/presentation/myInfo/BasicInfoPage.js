52b3a0ee1a71c9add1abb570ffd9e0e896949056
guswlsakfls <guswlsakfls@gmail.com>
2022-09-02 00:59:02 +0900

    Fix: Edit 버튼 누르면 회원정보 저장해 넘어가게만 만듬

--- frontend/kculterfe/src/myPage/presentation/myInfo/BasicInfoPage.js
+++ frontend/kculterfe/src/myPage/presentation/myInfo/BasicInfoPage.js
@@ -12,52 +12,50 @@ export default function EditBasicInfo() {
   const [email, setEmail] = useState(""); //이메일
   const [nickName, setNickName] = useState(''); //닉네임
   const [countryCode, setCountryCode] = useState('') //나라
+  const [countryLabel, setCountryLabel] = useState('') //나라
   const [age, setAge] = useState('');
   const [gender,setGender] = useState('');
   const [pfImg, setPfImg] = useState('');
+  const [formValue, setFormValue] = useState(''); // 폼 변경 변수
 
   const [memberInfo, setMemberInfo] = useState([]);
 
   /*오류메세지*/
-  const [verifyMessage, setVerMessage] = useState('');
   const [nickNameMessage, setnickNameMessage] = useState('');
 
   /*유효성 검사*/
-  const [emailOk, setEmailOk] = useState(false);
   const [isNickName, setIsNickName] = useState(false);
 
     // 값이 수정되어 버튼 활성화 위해
   const [countryCodeAvail, setCountryCodeAvail] = useState(false);
-  const [ageAvail, setageAvail] = useState(false);
   const [genderAvail, setGenderAvail] = useState(false);
   const [pfImageAvail, setPfImageAvail] = useState(false);
-  const [emailAvail,setEmailAvail] = useState(false);
   const [nickNameAvail,setNickNameAvail] = useState(false);
 
   /*체크 버튼 disabled만들기 위해 */
-  const [emailBtDis,setEmailBtDis] = useState(true);
+  // const [emailBtDis,setEmailBtDis] = useState(true);
   const [NickNameBtDis, setNicNameBtDis] = useState(true);
-
-  const formData = {
-    email : email,
-    nickName : nickName,
-    countryCode : countryCode,
-    age : age,
-    gender : gender,
-    pf_image : pfImg
-  }
+  const [checkDefaultGender, setCheckDefaultGender] = useState(false);
 
   useEffect(() => {
     getMemberInfo()
     .then(resData => {
-      console.log(resData);
+      // console.log('resData: ' + resData);
       setMemberInfo(resData)
+      // console.log('gender: ' + resData.entries().gender);
+      if (memberInfo.gender == "male") {
+        setCheckDefaultGender(false);
+      }
+      else {
+        setCheckDefaultGender(true);
+      }
     })
     .catch(err => {
       console.log(err);
     })
   },[]);
 
+  // 사진 파일 변경
   const onChangeProfilImg = (e) => {
     e.preventDefault();
     const imgUrl = e.target.files[0];
@@ -75,31 +73,11 @@ export default function EditBasicInfo() {
     }
   }
 
-  const onChangeEmail = (e) => {
-      let regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-      const emailRegex = e.target.value;
-      if(emailRegex === ""){
-        setEmailBtDis(true);
-        setVerMessage('');
-        setEmailOk(false);
-      }
-      else if (!regExp.test(emailRegex)) {
-        setEmailBtDis(true);
-        setVerMessage('Please verify your email');
-        setEmailOk(false);
-      }
-      else
-      {
-        setVerMessage('OK :)');
-        setEmailOk(true);
-        setEmailBtDis(false);
-        setEmail(emailRegex);
-      }
-  }
-
+  // 닉네임 변경
   const onChangeNickName = (e) => {
     let regExp = /[^a-zA-Z]/g
     const nickNameRegex = e.target.value;
+
     if(nickNameRegex === ""){
       setnickNameMessage("");
       setNicNameBtDis(true);
@@ -116,23 +94,6 @@ export default function EditBasicInfo() {
       setnickNameMessage("OK :)")
       setIsNickName(true);
       setNicNameBtDis(false);
-    }
-  }
-
-  // 이메일 중복 검사
-  const emaildupli = async () => {
-    if (email == "hyujo@gmail.com") { // 현재와 같은 이메일 일시
-      alert("This same email is available.")
-      setEmailAvail(true)
-      return ;
-    }
-    const result = await checkEmail(email);
-    if(result > 0){
-      alert("This email is not available.")
-      setEmailAvail(false);
-    }else{
-      alert("This email is available.")
-      setEmailAvail(true);
     }
   }
 
@@ -153,30 +114,48 @@ export default function EditBasicInfo() {
     }
   }
 
-  const checkAge = (e) => {
-    const result = e.target.value
-    setageAvail(true);
-    setAge(result);
-  }
-
   /* 나라선택 */
   const options = useMemo(() => countryList().getData(), []);
 
   const changeHandler = value => {
+    setCountryLabel(value.label);
     setCountryCode(value.value);
     setCountryCodeAvail(true);
   }
 
+  // 기본 정보 수정 버튼
   const editMember = async () => {
-    if (!emailAvail){ setEmail("hyujo@gmail.com"); } // 원래 이메일 또는 빈 이메일(빈 이메일 넣으면 put 할 때 통신하지 말아야함. 
-    if (!nickNameAvail){ setNickName("hyujo"); } // 현재 와 동일하게 해야함.
-    if (!ageAvail) { setAge("30"); }
-    if (!genderAvail) {setGender("male"); }
-    if (!countryCodeAvail) {setCountryCode("Korea"); }
-    if (!pfImageAvail) {setPfImg(""); } // 사진 넣어야함
-    
-    const result = await EditMemberInfo(formData);
-    if(result == 1){
+    var gender_ = gender;
+    var countryCode_ = countryCode;
+
+    if (!nickName){ setNickName("success"); } // 현재 와 동일하게 해야함.
+    if (gender == '') {gender_ = gender; }
+    if (countryCode == '') {countryCode_ = "TEST"; }
+    if (!pfImg) {setPfImg(""); } // 사진 넣어야함
+
+    const memberNumHash = window.sessionStorage.getItem("memberHash")
+
+    const formData = {
+      memberNumHash: memberNumHash,
+      nickName : nickName,
+      // 여기서 밑에 두 변수는 useState 변하고 값이 안들어 와서 따로 지역 변수로 선언해줌(다른 분들에게 물어봐야할 듯)
+      countryCode : countryCode_,
+      gender : gender_,
+    }
+
+    console.log(formData) // 잘 찍히는지 확인
+
+    const fmd = new FormData();
+    const jsonForm = JSON.stringify(formData);
+    const blobForm = new Blob([jsonForm],{
+        type : 'application/json'
+    })
+
+    fmd.append('formValue', blobForm);
+    fmd.append('file', '');
+
+    const result = await EditMemberInfo(fmd);
+    if(result == 200){
       alert("Success on edit");
     }
     else{
@@ -189,31 +168,17 @@ export default function EditBasicInfo() {
       <Form>
         {/* 사진 업로드 */}
         <Form.Group controlId="formFile" className="mb-3">
-          <img src={pfImg} alt="profile" width="100" height="130"></img>
+          <img src={pfImg || memberInfo.pf_image} alt="profile" width="100" height="130"></img>
           <Form.Label>Profile img upload</Form.Label>
           <Form.Control accept="image/jpg, image/png, image/jpeg" onChange={onChangeProfilImg} type="file" />
-        </Form.Group>
-
-        {/* 이메일 */}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" defaultValue={"hyujo@gmail.com"} onChange={onChangeEmail}/>
-          <span className={`message ${emailOk ? 'success' : 'error'}`}>{verifyMessage}</span>
-          <button type="button" disabled={emailBtDis} onClick={emaildupli} id='btn-check'>Check</button>
         </Form.Group>
 
         {/* 닉네임 */}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Nick-Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Your NickName" onChange={onChangeNickName} defaultValue={"hyujo"} />
+          <Form.Control type="text" placeholder="Enter Your NickName" onChange={onChangeNickName} defaultValue={memberInfo.nickName} />
           <span className={`message ${isNickName ? 'success' : 'error'}`}>{nickNameMessage}</span>
           <button type="button" disabled={NickNameBtDis} onClick={nicknamedupli} id='btn-check'>Check</button>
-        </Form.Group>
-
-        {/* 나이 */}
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>How old are you ?</Form.Label>
-          <Form.Control type="number" onChange={checkAge} placeholder="age" min="10" max="100" defaultValue={"30"} />
         </Form.Group>
 
         {/* 나라 */}
@@ -221,7 +186,7 @@ export default function EditBasicInfo() {
           <Form.Label>Contury</Form.Label>
           <Select 
             options={options} 
-            defaultValue={{ label: "Korea", value: "country" }}
+            value={{ label: countryLabel || memberInfo.countryCode }}
             onChange={changeHandler} >
           </Select>
         </Form.Group>
@@ -231,11 +196,11 @@ export default function EditBasicInfo() {
           <Form.Label>Gender</Form.Label>
           <div className="select-gender">
             {/* checked{gender === 'female'} 이렇게 넣어주면 초기값 완성 */}
-            <input type='radio' id = "select" name='gender' value='female' onClick={(e) => { 
-              setGender(e.target.value); setageAvail(true) }} />
+            <input type='radio' id = "select" name='gender' value='female' checked={checkDefaultGender ? false : true} onClick={(e) => { 
+              setGender(e.target.value); setGenderAvail(true); setCheckDefaultGender(false)}} /> {/* checked 초기값 줄 때 추후 리팩토링의 여지가 있다. */}
             <label for ="select">여성</label>
-            <input type='radio' id = "select2" name='gender' value='male' onClick={(e) => { 
-              setGender(e.target.value); setageAvail(true) }}/>
+            <input type='radio' id = "select2" name='gender' value='male' checked={checkDefaultGender ? true : false} onClick={(e) => { 
+              setGender(e.target.value); setGenderAvail(true); setCheckDefaultGender(true)}}/>
             <label for="select2">남성</label>
           </div>
         </Form.Group>
@@ -243,7 +208,7 @@ export default function EditBasicInfo() {
 
       {/* Edit 버튼 */}
       <button className="cp-btn" 
-        disabled={!(emailAvail||nickNameAvail||ageAvail||countryCodeAvail||genderAvail||pfImageAvail)} 
+        disabled={!(nickNameAvail||countryCodeAvail||genderAvail||pfImageAvail)} 
         onClick={editMember}>
           Edit
       </button>
