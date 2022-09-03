141846d3d73f89ae7e983f6cf9eb7055df1e4cde
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-09-03 18:02:17 +0900

    Update: 맵화면에 들어갔을 경우 sidebar 렌더링, 장소를 눌렀을 때 맵화면 확대, 코스 저장 모달 exit 버튼 생성 및 alert 창 생성, 콘서트화면 redux문제 해결, search 오류 해결, sidebar 엑박 렌더링 해결

--- frontend/kculterfe/src/layout/sidebar/mapPage/presentation/CourseCard.js
+++ frontend/kculterfe/src/layout/sidebar/mapPage/presentation/CourseCard.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { useDispatch } from "react-redux";
+import {
+	useDispatch
+} from "react-redux";
+import { CLICK_PLACE } from "../../../../redux/reducer";
 import {
 	handleOnClickDel
 } from '../container/handleCourseCard';
 import "../map-sidebar.css";
+
+function handleOnClickCard(item, dispatch) {
+	dispatch({
+		type: CLICK_PLACE,
+		data: item,
+	})
+}
 
 function CourseCard (props) {
 	const dispatch = useDispatch();
@@ -14,6 +24,7 @@ function CourseCard (props) {
 			props.courseList.map((item, i) => (
         <div className="course-card"
 					key={i}
+					onClick={() => handleOnClickCard(item, dispatch)}
 				>
 					<img
 						src={item.fileUrl}
